1144406268b21e7e2d6770a6c6d5d4be5bbd2cd6
Amrinder Singh <75622634+amrindersingh1820@users.noreply.github.com>
2026-07-05 13:05:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (31.25%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+vector<string> ans;
+vector<string> keypad ={
+    " ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+};
+
+void backtrack(string &digits, int index, string &curr){
+    if(index == digits.size()){
+        ans.push_back(curr); return;
+    }
+    string letters = keypad[digits[index] - '0'];
+    for(char ch : letters){
+        curr.push_back(ch);
+        backtrack(digits, index + 1, curr);
+        curr.pop_back();
+    }
+}
+    vector<string> letterCombinations(string digits) {
+     if(digits.empty()) return{};
+        string curr;
+        backtrack(digits, 0, curr);
+        return ans;
+       
+    }
+};
